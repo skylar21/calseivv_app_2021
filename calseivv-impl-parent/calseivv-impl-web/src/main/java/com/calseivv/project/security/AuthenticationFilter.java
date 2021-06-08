87475e1adf188339c32ccc9280b282c0cfc35a11
Francis Morota <francis.morota@gmail.com>
2021-06-08 23:31:28 +0800

    remove filter test

--- calseivv-impl-parent/calseivv-impl-web/src/main/java/com/calseivv/project/security/AuthenticationFilter.java
+++ calseivv-impl-parent/calseivv-impl-web/src/main/java/com/calseivv/project/security/AuthenticationFilter.java
@@ -25,24 +25,26 @@ public class AuthenticationFilter implements Filter {
             HttpSession ses = req.getSession(true);
             String reqURI = req.getRequestURI();
 
+            chain.doFilter(request, response);
+
             //fix redirect unknown URLs
-            if ((ses.getAttribute("username") != null && reqURI.equals("/")) || (ses.getAttribute("username") != null && reqURI.equals("/login.jsf"))
-                    || (ses.getAttribute("username") != null && reqURI.equals("/forget.jsf")) || (ses.getAttribute("username") != null && reqURI.equals("/registration.jsf"))) {
-                res.sendRedirect("/secured/home.jsf");
-            }
-
-            System.out.println(reqURI);
-
-            //allow user in publicly accessible paths
-            if (reqURI.indexOf("/login.jsf") >= 0 || reqURI.indexOf("/registration.jsf") >= 0 || reqURI.indexOf("/public/") >= 0
-                    || reqURI.indexOf("/resources") >= 0 || reqURI.indexOf("/forget.jsf") >= 0
-                    || reqURI.indexOf("/botdetectcaptcha") >= 0
-                    || (ses != null && ses.getAttribute("username") != null)
-                    || reqURI.contains("javax.faces.resource")) {
-                chain.doFilter(request, response);
-            } else {
-                res.sendRedirect("/login.jsf"); // Anonymous user. Redirect to login page
-            }
+//            if ((ses.getAttribute("username") != null && reqURI.equals("/")) || (ses.getAttribute("username") != null && reqURI.equals("/login.jsf"))
+//                    || (ses.getAttribute("username") != null && reqURI.equals("/forget.jsf")) || (ses.getAttribute("username") != null && reqURI.equals("/registration.jsf"))) {
+//                res.sendRedirect("/secured/home.jsf");
+//            }
+//
+//            System.out.println(reqURI);
+//
+//            //allow user in publicly accessible paths
+//            if (reqURI.indexOf("/login.jsf") >= 0 || reqURI.indexOf("/registration.jsf") >= 0 || reqURI.indexOf("/public/") >= 0
+//                    || reqURI.indexOf("/resources") >= 0 || reqURI.indexOf("/forget.jsf") >= 0
+//                    || reqURI.indexOf("/botdetectcaptcha") >= 0
+//                    || (ses != null && ses.getAttribute("username") != null)
+//                    || reqURI.contains("javax.faces.resource")) {
+//                chain.doFilter(request, response);
+//            } else {
+//                res.sendRedirect("/login.jsf"); // Anonymous user. Redirect to login page
+//            }
         } catch (Exception t) {
             t.printStackTrace();
         }
